68614e5eece603760ae5fb58351f1a62e359b6c0
apoorvaa015 <sharmaapoorva0103@gmail.com>
2026-06-30 22:38:22 +0530

    Configure app for Render deployment

--- server.js
+++ server.js
@@ -141,6 +141,8 @@ app.post('/delete/:id', (req, res) => {
     );
 });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
